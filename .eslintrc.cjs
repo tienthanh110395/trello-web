120331dd5104c43fa361ae0a76c03fa89abae6bc
DESKTOP-KU6C77T\HP-1 <thanhtt>
2023-12-19 15:13:17 +0700

    commit Import rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,7 +8,7 @@ module.exports = {
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended'
   ],
-  
+
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
   plugins: [
