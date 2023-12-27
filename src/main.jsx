8f953c8b70ff442c52a24f51112c7526a74035e1
DESKTOP-KU6C77T\HP-1 <thanhtt>
2023-12-27 18:17:22 +0700

    add theme

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import CssBaseline from '@mui/material/ScopedCssBaseline'
 import App from './App.jsx'
-import './index.css'
+import { ThemeProvider } from '@mui/material/styles'
+import theme from './theme'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CssBaseline />
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 )
